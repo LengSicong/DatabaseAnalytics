42b592e4e0a1fb7f328dd11b303dc2cabbb077f0
Jiaxi Li <li.jiaxi97@gmail.com>
2020-11-27 02:39:53 +0800

    Add SparkSession

--- tfidf.py
+++ tfidf.py
@@ -2,6 +2,7 @@ from pyspark.sql import SQLContext
 from pyspark.mllib import *
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.ml.feature import CountVectorizer, IDF, Tokenizer
+from pyspark.sql import SparkSession
 
 session = SparkSession.builder.appName("tfidf").getOrCreate()
 sc = session.sparkContext
@@ -17,15 +18,18 @@ schema = StructType([
     StructField("reviewText", StringType(), True),
     StructField("createdAt", StringType(), True)])
 
-reviews = session.read.csv("hdfs:///DBProject/review.csv", header=False, sep=",", schema=schema)
-reviews = reviews.na.drop(subset=["reviewText"])    # drop the reviews with NA reviewText
+reviews = session.read.csv(
+    "hdfs:///DBProject/review.csv", header=False, sep=",", schema=schema)
+# drop the reviews with NA reviewText
+reviews = reviews.na.drop(subset=["reviewText"])
 
 # convert reviewText to words
 tokenizer = Tokenizer(inputCol="reviewText", outputCol="reviewWords")
 tokenized_reviews = tokenizer.transform(reviews)
 
 # get tf
-cv = CountVectorizer(inputCol="reviewWords", outputCol="rawFeatures", vocabSize=20)
+cv = CountVectorizer(inputCol="reviewWords",
+                     outputCol="rawFeatures", vocabSize=20)
 model = cv.fit(tokenized_reviews)
 featurizedData = model.transform(tokenized_reviews)
 
@@ -35,8 +39,11 @@ idfModel = idf.fit(featurizedData)
 rescaledData = idfModel.transform(featurizedData)
 
 vocab = model.vocabulary
+
+
 def extract_values(vector):
     return {vocab[i]: float(tfidf) for (i, tfidf) in zip(vector.indices, vector.values)}
+
 
 def save_as_string(vector):
     words = ""
@@ -45,10 +52,12 @@ def save_as_string(vector):
         words += temp
     return words[:-2]
 
+
 output = rescaledData.select('reviewerID', 'asin', 'createdAt', 'features').rdd.map(
     lambda x: [x[0], x[1], x[2], save_as_string(x[3])])
 
-output_df = session.createDataFrame(output, ['reviewerID', 'asin', 'createdAt', 'tfidf'])
+output_df = session.createDataFrame(
+    output, ['reviewerID', 'asin', 'createdAt', 'tfidf'])
 output_df.write.format("csv").save("hdfs:///DBProject/tfidf_output.csv")
 output_df.show(1)
 session.stop()
